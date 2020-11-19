d25576f3b70906369f22308549c243ceda714822
Sigurd Eckermann <sigurd.eckermann@yahoo.de>
2020-11-19 15:11:26 +0100

    material integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
+// import {MatButtonModule} from 'material/button';
+// import IconButton from "@material-ui/core/IconButton";
+// import { MatButtonModule } from "@angular/material";
+import { CustomMaterialModule } from "./material.module";
+//import { MatSliderModule } from "@angular/material/slider";
+//import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, CustomMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
