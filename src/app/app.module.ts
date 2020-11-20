718274ba0145f71cb1b685b8763a9d9089de06fa
Sigurd Eckermann <sigurd.eckermann@yahoo.de>
2020-11-20 13:08:52 +0100

    some tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 
 import { AppComponent } from "./app.component";
@@ -11,7 +12,7 @@ import { CustomMaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, CustomMaterialModule],
+  imports: [BrowserModule, BrowserAnimationsModule, CustomMaterialModule],
   providers: [],
   bootstrap: [AppComponent]
 })
